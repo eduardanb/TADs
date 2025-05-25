893ca2da43ae6a0c8b6f562b1af5847b369f2a52
Maria Eduarda da Nóbrega <eduardanob2005@gmail.com>
2025-05-25 20:56:31 -0300

    Update Pilha

--- RoteiroEDA_TAD-2/RoteiroEDA_TAD-2/src/tad/pilha/MinhaPilha.java
+++ RoteiroEDA_TAD-2/RoteiroEDA_TAD-2/src/tad/pilha/MinhaPilha.java
@@ -1,42 +1,61 @@
 package tad.pilha;
 
 public class MinhaPilha implements PilhaIF<Integer> {
-	
-	private int tamanho = 10;
-	private Integer[] meusDados = null;
+    
+    private int tamanho = 10;
+    private Integer[] meusDados;
+    private int topo = -1;
 
-	public MinhaPilha(int tamanho) {
-		this.tamanho = tamanho;
-	}
-	
-	public MinhaPilha() {
-	}
+    public MinhaPilha(int tamanho) {
+        this.tamanho = tamanho;
+        this.meusDados = new Integer[tamanho];
+    }
+    
+    public MinhaPilha() {
+        this.meusDados = new Integer[tamanho];
+    }
 
-	@Override
-	public void empilhar(Integer item) {
-		throw new UnsupportedOperationException("Implementar");
-		
-	}
+    @Override
+    public void empilhar(Integer item) {
+        if (isFull()) {
+            throw new RuntimeException("Pilha cheia");
+        }
+        meusDados[++topo] = item;
+    }
 
-	@Override
-	public Integer desempilhar() {
-		throw new UnsupportedOperationException("Implementar");
-	}
+    @Override
+    public Integer desempilhar() {
+        if (isEmpty()) {
+            throw new RuntimeException("Pilha vazia");
+        }
+        Integer valor = meusDados[topo];
+        meusDados[topo--] = null;
+        return valor;
+    }
 
-	@Override
-	public Integer topo() {
-		throw new UnsupportedOperationException("Implementar");
-	}
+    @Override
+    public Integer topo() {
+        if (isEmpty()) {
+            return null;
+        }
+        return meusDados[topo];
+    }
 
-	@Override
-	public PilhaIF<Integer> multitop(int k) {
-		throw new UnsupportedOperationException("Implementar");
-	}
+    @Override
+    public PilhaIF<Integer> multitop(int k) {
+        MinhaPilha nova = new MinhaPilha(k);
+        for (int i = topo, count = 0; i >= 0 && count < k; i--, count++) {
+            nova.empilhar(meusDados[i]);
+        }
+        return nova;
+    }
 
-	@Override
-	public boolean isEmpty() {
-		throw new UnsupportedOperationException("Implementar");
-	}
+    @Override
+    public boolean isEmpty() {
+        return topo == -1;
+    }
 
-
+    public boolean isFull() {
+        return topo == tamanho - 1;
+    }
 }
