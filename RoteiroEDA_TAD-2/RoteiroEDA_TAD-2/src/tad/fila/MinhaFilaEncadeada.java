1015a0771e9bd5518d4a955a91f002d067fd3703
Maria Eduarda da Nóbrega <eduardanob2005@gmail.com>
2025-05-25 20:47:32 -0300

    Update Fila

--- RoteiroEDA_TAD-2/RoteiroEDA_TAD-2/src/tad/fila/MinhaFilaEncadeada.java
+++ RoteiroEDA_TAD-2/RoteiroEDA_TAD-2/src/tad/fila/MinhaFilaEncadeada.java
@@ -2,40 +2,67 @@ package tad.fila;
 
 public class MinhaFilaEncadeada implements FilaIF<Integer> {
 
-	@Override
-	public void enfileirar(Integer item) throws FilaCheiaException {
-		// TODO Auto-generated method stub
-		
-	}
+    private class Node {
+        Integer valor;
+        Node prox;
 
-	@Override
-	public Integer desenfileirar() throws FilaVaziaException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        Node(Integer valor) {
+            this.valor = valor;
+            this.prox = null;
+        }
+    }
 
-	@Override
-	public Integer verificarCauda() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private Node cabeca = null;
+    private Node cauda = null;
 
-	@Override
-	public Integer verificarCabeca() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void enfileirar(Integer item) throws FilaCheiaException {
+        Node novo = new Node(item);
+        if (isEmpty()) {
+            cabeca = novo;
+            cauda = novo;
+        } else {
+            cauda.prox = novo;
+            cauda = novo;
+        }
+    }
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public Integer desenfileirar() throws FilaVaziaException {
+        if (isEmpty()) {
+            throw new FilaVaziaException();
+        }
+        Integer valor = cabeca.valor;
+        cabeca = cabeca.prox;
+        if (cabeca == null) {
+            cauda = null;
+        }
+        return valor;
+    }
 
-	@Override
-	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public Integer verificarCauda() {
+        if (cauda != null) {
+            return cauda.valor;
+        }
+        return null;
+    }
 
+    @Override
+    public Integer verificarCabeca() {
+        if (cabeca != null) {
+            return cabeca.valor;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return cabeca == null;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
 }
